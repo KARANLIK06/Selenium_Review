89089b4d1b747f2113c4f7a14c401b34c9d82ef7
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-08 00:32:19 +0300

    week01

---
+++ src/test/java/week01/FirstSeleniumClass.java
@@ -0,0 +1,24 @@
+package week01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstSeleniumClass {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.edgedriver().setup();
+
+
+        WebDriver driverChrome=new ChromeDriver();
+        WebDriver driverEdge= new EdgeDriver();
+        driverChrome.manage().window().maximize();
+        driverEdge.manage().window().maximize();
+
+        driverChrome.get("https://www.google.com");
+        driverEdge.get("https://practice.cydeo.com");
+    }
+}
