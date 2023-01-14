30051e8a7827ca772f2d45a94657cac13d46d3be
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-14 19:57:03 +0300

    week03_cssSelector_xpath

---
+++ src/test/java/week03/XpathLocators.java
@@ -0,0 +1,30 @@
+package week03;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathLocators {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://practice.cydeo.com/dropdown");
+
+        // xpath is another custom locators
+        // we have two types of syntax of xpath
+        // 1. absolute xpath
+        // it starts with single slash ( / ), starts from html tag, go down the element you want to locate , step by step
+
+
+        //2. relative xpath
+        //    //tagName[@attributeName='value']
+        // frequently used syntax
+        /*
+            //tagName[contains(@attribute, 'value’)]
+//ta        tagName[.='text'] (same as //tagName[text()='text’], . represents text)
+//*[        //*[@attribute='value’] ( use * when we don't want use tag name)
+         */
+    }
+}
