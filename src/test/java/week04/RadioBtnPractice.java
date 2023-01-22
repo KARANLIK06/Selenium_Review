18bc4035a92924c0dc26213211f70f6d7e08515d
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-22 14:30:54 +0300

    week04

---
+++ src/test/java/week04/RadioBtnPractice.java
@@ -0,0 +1,17 @@
+package week04;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class RadioBtnPractice {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://www.tutorialstonight.com/html-radio-button");
+    }
+}
