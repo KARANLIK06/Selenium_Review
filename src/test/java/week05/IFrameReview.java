0ee594aeb367665e1f42d5b4d40e28e8ab188f00
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-25 19:32:06 +0300

    week05

---
+++ src/test/java/week05/IFrameReview.java
@@ -0,0 +1,50 @@
+package week05;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class IFrameReview extends TestBase{
+
+
+
+
+    // providing iframe id
+    @Test
+    public void test1(){
+        driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert");
+        driver.switchTo().frame("iframeResult");
+        driver.findElement(By.xpath("//button[@onClick='myFunction()']")).click();
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
+    }
+
+
+    // locage iframe, then provide the web element
+
+    @Test
+    public void test2(){
+        driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert");
+        WebElement iframe = driver.findElement(By.name("iframeResult"));
+        driver.switchTo().frame(iframe);
+        driver.findElement(By.xpath("//button[@onClick='myFunction()']")).click();
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
+    }
+
+
+    // provide index of that frame
+    @Test
+    public void test3(){
+        driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert");
+        driver.switchTo().frame(1);
+        driver.findElement(By.xpath("//button[@onClick='myFunction()']")).click();
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
+    }
+
+}
