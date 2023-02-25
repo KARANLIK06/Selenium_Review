24e59679913d0b1817866408a9f6c5c9d06b26f4
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-25 20:01:49 +0300

    week06

---
+++ src/test/java/week06/JavaFaker.java
@@ -0,0 +1,22 @@
+package week06;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
+public class JavaFaker {
+    Faker faker = new Faker();
+    @Test
+    public void test1(){
+
+
+        String name1 = faker.name().nameWithMiddle();
+        String name2 = faker.name().username();
+        String address = faker.address().country() + faker.address().cityName() + faker.address().fullAddress();
+        String email = faker.internet().emailAddress();
+        int age = faker.number().numberBetween(18,80);
+
+        String actorName = faker.chuckNorris().fact();
+        System.out.println(actorName);
+
+    }
+}
