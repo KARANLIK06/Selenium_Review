f9e610e8081ad89951ba885b92a67d0534fe18ed
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-25 23:57:58 +0300

    week06

--- src/test/java/week06/JSExecuterTest.java
+++ src/test/java/week06/JSExecuterTest.java
@@ -10,16 +10,19 @@ public class JSExecuterTest extends TestBase {
     @Test
     public void test1() throws InterruptedException {
         driver.get("https://www.google.com/");
-        WebElement search = driver.findElement(By.xpath("(//input[@name='btnK'])[2]"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()",search);
+        WebElement googleSearch = driver.findElement(By.xpath("(//input[@name='btnK'])[2]"));
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].click()",googleSearch);
         Thread.sleep(2000);
-
     }
 
     @Test
-    public void test2(){
-
+    public void test2() throws InterruptedException {
+        driver.get("http://practice.cybertekschool.com/dynamic_controls");
+        WebElement input = driver.findElement(By.xpath("//form[@id='input-example']/input"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].setAttribute('value', 'Cydeo10')",input);
+        Thread.sleep(2000);
 
     }
 }
