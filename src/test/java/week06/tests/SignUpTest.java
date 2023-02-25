881ae419facfce6b88b21500914a11dd4b4a057e
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-25 23:54:37 +0300

    week06

--- src/test/java/week06/tests/SignUpTest.java
+++ src/test/java/week06/tests/SignUpTest.java
@@ -1,16 +1,25 @@
 package week06.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.Driver;
+import week06.pages.ConfirmationPage;
 import week06.pages.SignUpPage;
 
 public class SignUpTest {
-    SignUpPage pageObject =new SignUpPage();
+    ConfirmationPage confirmationPage= new ConfirmationPage();
+    SignUpPage signUpPage =new SignUpPage();
     @Test
     public void test1(){
         Driver.getDriver().get("http://practice.cybertekschool.com/sign_up");
-        pageObject.nameInput.sendKeys("John");
-        pageObject.emailInput.sendKeys("abc@abc.com");
-        pageObject.btn.click();
+        //signUpPage.nameInput.sendKeys("John");
+        //signUpPage.emailInput.sendKeys("abc@abc.com");
+        //signUpPage.btn.click();
+        signUpPage.login("John","abc@abc.com");
+
+        String actualText=confirmationPage.text.getText();
+        String expectedText="Thank you for signing up. Click the button below to return to the home page.";
+        Assert.assertEquals(actualText,expectedText);
+        Driver.closeDriver();
     }
 }
